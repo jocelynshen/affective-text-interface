7b8a1c7c25f45f0ddb62aa210273e080d8cd091b
Kathryn Jin <kjkjkathryn@gmail.com>
2022-04-03 18:31:15 -0400

    get text samples and map features to css styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import {Recorder} from 'react-voice-recorder'
 import 'react-voice-recorder/dist/index.css'
 import axios from '../node_modules/axios';
- 
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container } from 'react-bootstrap';
+
 class App extends Component {
   constructor(props) {
     super(props)
  
     this.state = {
+      textSamples: [],
       audioDetails: {
         url: null,
         blob: null,
@@ -16,12 +21,27 @@ class App extends Component {
           h: null,
           m: null,
           s: null,
-          }
         }
+      },
+      outputData: [],
     }
   }
+
+  componentDidMount() {
+    const self = this;
+    axios.get("http://127.0.0.1:5000/textSamples/")
+    .then(function (response) {
+      // handle success
+      self.setState({textSamples: response.data.textSamples})
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }
  
-  handleAudioStop(data){
+  handleAudioStop(data) {
     console.log(data)
     this.setState({ audioDetails: data });
   }
@@ -37,6 +57,7 @@ class App extends Component {
     //     // handle error
     //     console.log(error);
     //   })
+    const self = this;
     const formData = new FormData();
     formData.append("audioFile", file);
     axios.post("http://127.0.0.1:5000/upload/", formData, {
@@ -44,15 +65,17 @@ class App extends Component {
         'Content-Type': 'multipart/form-data'
       }
     })
-      .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      self.setState({outputData: response.data.output});
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
   }
+
   handleReset() {
     const reset = {
       url: null,
@@ -66,14 +89,43 @@ class App extends Component {
     }
     this.setState({ audioDetails: reset });
   }
- 
+
+  generateStyle(features) {
+    const labelToColor = {
+      "neutral": "black",
+      "calm": "cornflowerblue",
+      "happy": "gold",
+      "sad": "blue",
+      "angry": "maroon",
+      "fear": "red",
+      "disgust": "olive",
+      "ps": "deeppink",
+      "boredom": "gray"
+    }
+
+    const style = {};
+    const loudness = features.loudness;
+    const fontSize = `${Math.round(loudness * 72)}pt`;
+    style.fontSize = fontSize;
+    style.color = labelToColor[features.emotion_label];
+    style.opacity = features.confidence;
+    return style
+  }
+
   render() {
- 
     return (
-      <React.Fragment>    
+      <React.Fragment> 
+        <Tabs className="m-3">        
+          {this.state.textSamples.map((text, i) => (
+            <Tab className="m-3" eventKey={i} key={i} title={`Sample ${i}`}>
+              {text}
+            </Tab>
+          ))}
+        </Tabs>
+
         <Recorder
           record={true}
-          title={"Record audio (1 minute limit)"}
+          title={"IM FINE"}
           audioURL={this.state.audioDetails.url}
           showUIAudio
           handleAudioStop={data => this.handleAudioStop(data)}
@@ -81,11 +133,22 @@ class App extends Component {
           handleAudioUpload={data => this.handleAudioUpload(data)}
           handleReset={() => this.handleReset()}
           // mimeTypeToUseWhenRecording={'audio/wave'}
+          hideHeader
         />
-        <div>
-          
-        </div>
+        <Container fluid>
+          Result
+          <br/>
+          {this.state.outputData.map((entry, i) =>
+            (
+              <span key={i} style={this.generateStyle(entry.features)}>
+                {` ${entry.text} `}
+              </span>
+            )
+          )}
+        </Container>
+        
       </React.Fragment>
+
     )
   }
 }
